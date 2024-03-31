12be6a6e97144d2135e9a71d0b16278882fdeaf2
FrancoRongo <franco.rongo98@hotmail.com>
2024-03-31 20:56:44 -0300

    index.js

--- FrontEnd/scripts/index.js
+++ FrontEnd/scripts/index.js
@@ -1,17 +1,13 @@
 const axios = require("axios");
 const renderBook = require("./renderBook");
-//const apiUrl= "http://localhost:3000/books";
-const tempData = require ("./tempData");
+const apiUrl= "http://localhost:3000/books";
 
-tempData.forEach(book => {
-  renderBook(book);
-});
-/*axios.get(apiUrl)
+axios.get(apiUrl)
   .then(response => {
      rendersCards(response.data);
   })
   .catch(error => {
     console.error('Error al obtener datos:', error);
   });
-*/
+
 
